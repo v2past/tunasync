2a709e20902e3678e637828ac1ee7ae56236c8fe
v2past <mycloudou@gmail.com>
2023-05-04 12:15:39 +0800

    Update tunusync.sh

--- tunusync.sh
+++ tunusync.sh
@@ -15,5 +15,18 @@ https://github.com/tuna/tunasync/blob/master/docs/zh_CN/get_started.md
 https://github.com/tuna/mirror-web
 https://github.com/iori2333/Mira
 
+https://github.com/weyo/mirrors
+
 脚本：
 https://github.com/tuna/tunasync-scripts
+
+
+部署参考：
+https://ring0.me/2013/09/how-ustc-mirror-works/
+https://blog.whsir.com/post-5094.html
+http://weyo.me/pages/techs/how-to-make-a-mirror-site/
+https://yangfannie.com/1643.html
+
+https://bookgh.github.io/posts/eafe.html
+https://blog.lussac.net/archives/342/
+
