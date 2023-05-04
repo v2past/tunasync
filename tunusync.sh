3e9b7c05fdbf1a0b196de385433cf76230517954
v2past <mycloudou@gmail.com>
2023-05-04 10:29:47 +0800

    Create tunusync.sh

---
+++ tunusync.sh
@@ -0,0 +1,16 @@
+参照链接
+https://mirrors.swjtu.edu.cn/
+
+*https://mirrors.ustc.edu.cn/
+*https://mirrors.sjtug.sjtu.edu.cn/
+https://mirrors.pku.edu.cn/Help/Ubuntu
+https://mirrors.nju.edu.cn/
+
+
+后端：
+https://github.com/tuna/tunasync/blob/master/docs/zh_CN/get_started.md
+
+前端：
+https://github.com/tuna/mirror-web
+https://github.com/iori2333/Mira
+
